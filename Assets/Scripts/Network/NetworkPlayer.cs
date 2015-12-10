7a54ff8a1409a21d1afd6f8cc38d46c9b41aff15
Guarionex <edwinom@iastate.edu>
2015-12-10 15:45:58 -0600

    Online player controller
    Ninja Controller now only controls a single player and not all players

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkPlayer : MonoBehaviour {
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+	//public GameObject myCamera;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		if(photonView.isMine)
+		{
+			//myCamera = GameObject.Find("../My Camera");
+			//myCamera.SetActive(true);
+			GetComponent<NinjaControllerScript>().enabled = true;
+		}
+		else 
+		{
+
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
